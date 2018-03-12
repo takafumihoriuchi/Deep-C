83600b9bda1a334bec8c6474e307129b20ce1039
Takafumi Horiuchi <hawkletter080e@icloud.com>
2018-03-12 23:24:41 +0900

    # Fix fixed indentation and space

--- mnist/mnist.c
+++ mnist/mnist.c
@@ -7,16 +7,16 @@ int main(void)
 	// train label -> train_label[60000]      (type: int)
 	// test image  -> test_image[10000][784]  (type: double, normalized, flattened)
 	// test label  -> test_label[10000]       (type: int)
-    read_mnist();
+	read_mnist();
 
-    // print the pixel values of mnist images to stdout
-    print_mnist_pixel(test_image, NUM_TEST);
-    // print the label of mnist data to stdout
-    print_mnist_label(test_label, NUM_TEST);
+	// print the pixel values of mnist images to stdout
+	print_mnist_pixel(test_image, NUM_TEST);
+	// print the label of mnist data to stdout
+	print_mnist_label(test_label, NUM_TEST);
 
-    // argument1: image array
-    // argument2: index of image to save
-    save_mnist_pgm(train_image, 0);
+	// argument1: image array
+	// argument2: index of image to save
+	save_mnist_pgm(train_image, 0);
 
-    return 0;
+	return 0;
 }
